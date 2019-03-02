3ab5c514a3aa6162119b0c1eed1a7f9ae7636216
livalex <livalex11@gmail.com>
2019-03-02 19:02:02 +0200

    added migrations and seeder

---
+++ database/factories/NewsCategoryFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\NewsCategory::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'slug' => $faker->slug,
+    ];
+});
---
+++ database/seeds/NewsCategorySeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\NewsCategory;
+
+class NewsCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    	// $categories = [];
+
+    	// for ($i = 0; $i < 10; $i++) {
+    	// 	$categories[] = [
+    	// 		'name' => 'Category ' . $i,
+    	// 		'slug' => 'category' . $i
+    	// 	];
+    	// }
+
+
+     //    DB::table('news_categories')->insert($categories);
+
+    	factory(NewsCategory::class, 5000)->create();
+    }
+}
